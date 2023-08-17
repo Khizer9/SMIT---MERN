bb87a0ef690c43b45b3773d79c8576d89e222a52
Khizer Ali <khizer358@gmail.com>
2023-08-17 13:02:36 +0500

    javascript

---
+++ Math Expression/index.js
@@ -0,0 +1,29 @@
+// Write a program that take two numbers & add them in a
+// new variable. Show the result in your browser.
+
+// var num1 = prompt("Enter First Number")
+// num1 = parseFloat(num1)
+// var num2 = prompt("Enter Second Number")
+// num2 = parseFloat(num2)
+
+// var result = num1 + num2;
+// alert("Your Result is: " + result);
+
+
+// Cost of one movie ticket is 600 PKR. Write a script to
+// store
+// ticket price in a variable & calculate the cost of buying 5
+// tickets
+// to a movie. Example output:
+
+// var singleMovieTicket = 600;
+// var fiveMovieTicket = singleMovieTicket * 5
+// alert("Total Cost to buy 5 ticket is: " + fiveMovieTicket)
+
+// Write a script to display multiplication table of any
+// number in your browser.
+
+var multiplicationNumber = prompt("Enter a Number for gerating table")
+multiplicationNumber = parseFloat(multiplicationNumber)
+
+console.log(multiplicationNumber + "X" + 1 + "=" + multiplicationNumber * 1)
