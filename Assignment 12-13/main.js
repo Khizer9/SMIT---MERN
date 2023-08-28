17eea4333f27a0b9f67f966a48f9b4314cd6d44f
Khizer Ali <khizer358@gmail.com>
2023-08-28 13:09:38 +0500

    assignment 12-13

---
+++ Assignment 12-13/main.js
@@ -0,0 +1,97 @@
+// Assignment 1
+// var int = prompt('Enter Number or Anything')
+// console.log(int)
+
+// if(int === ''){
+//     alert('Please enter something')
+// } else if(int === Number){
+//     alert('You have entered a number')
+// }else {
+//     alert('You entered a string')
+// }
+
+
+// Assignment 2
+// var num1 = prompt('Enter First Number')
+// var num2 = prompt('Enter Second Number')
+
+// if(num1 > num2){
+//     alert('Number 1 is Greater than Number 2')
+// } else if(num1 < num2){
+//     alert('Number 2 is Greater than Number 1')
+// }else{
+//     alert('Both numbers are same')
+// }
+
+// Assignment 3
+// var num = prompt('Enter a number')
+
+// if(num % 2 == 0 && num !== 0){
+//     alert('You have entered Even Number')
+// } else if(num % 2 == 1){
+//     alert('You have entered Odd Number')
+// } else if(num ===  0){
+//     alert('You have entered Zero Number')
+// }
+
+// Assignment 4
+// var vowels = 'aeiouAEIOU'
+// var isVowel = false;
+
+// var letter = prompt('Enter a letter')
+
+// for (var i = 0; i < vowels.length; i++) {
+//     if (letter === vowels[i]) {
+//         isVowel = true;
+//         break;
+//     }else {
+//         break
+//     }
+// }
+
+
+// if(isVowel){
+//     alert('Vowel found')
+// }else{
+//     alert('Vowel not found')
+// }
+
+
+// Assginment 5
+
+// var password = 'hereispassword'
+// var userPassword = prompt('Enter your password')
+
+// if(userPassword == password){
+//     alert('Password Matched')
+// } else if(userPassword == ''){
+//     alert('Enter a Password')
+// }else{
+//     alert('Please enter a valid password')
+// }
+
+// Assignment 6
+
+// var greeting;
+// var hours = 13
+
+// if(hours < 18){
+//     greeting = 'Have a good day!'
+// }else {
+//     greeting = 'Have a good Evening'
+// }
+// console.log(greeting)   
+
+// Assignment 7
+// var hour24 = prompt('Enter a time in 24 hour format for conversion')
+
+// if(hour24 > '0000' &&  hour24 < 1200){
+//     alert('Good Morning!')
+// }else if(hour24 >= 1200 && hour24 < 1700){
+//     alert('Good Afternoon!')
+// }else if(hour24 >= 1700 && hour24 < 2100){
+//     alert('Good Evening!')
+// }else {
+//     alert('Good Night!')
+// }
+
