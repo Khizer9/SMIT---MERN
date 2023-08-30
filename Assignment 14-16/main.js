29764772b6b9f53bbe353fa0fdd31f77a7d9c727
Khizer Ali <khizer358@gmail.com>
2023-08-31 00:15:52 +0500

    assignemnt 14-16

---
+++ Assignment 14-16/main.js
@@ -0,0 +1,17 @@
+// // Array Practice 14-16
+
+// var studentNameArray = []
+
+// var studentNameArrString = ['']
+
+// var stringArray = ['khizer', 'ali', 'haider']
+
+// var numberArray = [ 1, 2, 3, 4]
+
+// var booleanArray = [ true, false]
+
+// var mixedArray = [1, 2, 'khizer', false]
+
+// var qualArray = ['SSC', 'HSC', 'BSC', 'BS', 'BCOM', 'MS', 'M.PHIL', 'PHD']
+
+// console.log(qualArray)
