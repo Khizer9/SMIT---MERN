139da9f2ac86558af841f23ab70bde1c42d5d148
Khizer Ali <khizer358@gmail.com>
2023-09-19 13:58:03 +0500

    Assignment 31-35

--- Assignment 26-30/main.js
+++ Assignment 26-30/main.js
@@ -49,3 +49,14 @@
 //     alert(`You guess the wrong number, ${computerNum}`)
 // }
 
+// const number = parseInt(prompt('Enter the number of terms: '));
+// let n1 = 0, n2 = 1, nextTerm;
+
+// console.log('Fibonacci Series:');
+
+// for (let i = 1; i <= number; i++) {
+//     console.log(n1);
+//     nextTerm = n1 + n2;
+//     n1 = n2;
+//     n2 = nextTerm;
+// }
---
+++ Assignment 31-35/main.js
@@ -0,0 +1,92 @@
+// Assignment 1
+// var date = new Date()
+// console.log(date, 'date')
+
+// Assignment 2
+// const month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+// var d = new Date()
+// var currentMonth = month[d.getMonth()]
+// alert(currentMonth, 'currentMonth')
+
+// Assignment 3
+// var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+
+// var d = new Date()
+// var currentDay = days[d.getDay()].slice(0,3)
+// alert(`Today is ${currentDay}`)
+
+// Assignment 4
+// var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+
+// var d = new Date()
+// var currentDay = days[d.getDay()]
+// if(currentDay === 'Saturday' || currentDay === 'Sunday'){
+//     alert('Its a Fun Day')
+// }else {
+//     alert(`${currentDay}, Its not a fun day`)
+// }
+
+// Assignment 5
+// var d = new Date()
+// var currentDate = d.getDate()
+// console.log(currentDate, "currentDate")
+// if(currentDate <= 15){
+//     alert('First Fifteen days of month')
+// }else {
+//     alert('Last fifteen days of month')
+// }
+
+// Assignment 7
+// var d = new Date();
+// var currentTime = d.getHours()
+// if(currentTime >= 12){
+//     alert('its PM')
+// }else{
+//     alert('its AM')
+// }
+// console.log(currentTime, "currentTime")
+
+// Assignment 8
+// var laterDate = new Date("Dec 31, 2020 00:00:00")
+// console.log(laterDate)
+
+// // Assignment 9
+// var d = new Date("June 18, 2015")
+// var date = new Date()
+// console.log(date)
+
+// Assignment 13
+// var userAge = prompt('Enter your age')
+
+// var currentYear = new Date().getFullYear()
+
+// var yearOfBorn = currentYear - userAge
+// alert(`Year of Born is ${yearOfBorn}`)
+
+
+// Assignment 14
+// const month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+// var customerName = prompt('Enter your Name')
+// var d = new Date()
+// var currentMonth = month[d.getMonth()]
+// var NumOfUnits = +prompt('Enter Number of Units')
+// var chargePerUnit = 16
+// var lastPaySurcharge = 350
+
+// var netAmount = NumOfUnits * chargePerUnit
+// var grossAmount = netAmount + lastPaySurcharge
+
+// console.log(`
+//     Customer Name: ${customerName}
+//     Current Month :${currentMonth}
+//     Number of Units: ${NumOfUnits}
+//     Charge Per Unit: ${chargePerUnit}
+
+//     Net Amount Payable (Within Due Date): ${netAmount}
+//     Late Payment Surcharge: ${lastPaySurcharge}
+//     Gross Amount Payable (After Due Date): ${grossAmount}
+// `)
+
+
+
+
